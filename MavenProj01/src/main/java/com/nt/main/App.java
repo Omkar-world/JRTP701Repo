048f13cee91c9e21aa6b6a2d294c1bc02010e995
ASUS <ASUS@LAPTOP-CO5BIT8L>
2024-12-29 23:58:15 +0530

    dev1 commit

--- MavenProj01/src/main/java/com/nt/main/App.java
+++ MavenProj01/src/main/java/com/nt/main/App.java
@@ -4,6 +4,9 @@ package com.nt.main;
  * Hello world!
  */
 public class App {
+	public int sum(int a,int b) {
+		return a+b;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
