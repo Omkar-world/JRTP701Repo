534605d3ad3306c1b3130366f41cb68c27a3038e
Omkar-world <omkarverma8052979256@gmail.com>
2024-12-29 23:59:58 +0530

    TL add one more metthod

--- MavenProj01/src/main/java/com/nt/main/App.java
+++ MavenProj01/src/main/java/com/nt/main/App.java
@@ -7,6 +7,9 @@ public class App {
 	public int sum(int a,int b) {
 		return a+b;
 	}
+	public int sub(int a,int b) {
+		return a-b;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
