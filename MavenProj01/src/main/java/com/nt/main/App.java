e0dffe17c4793c3e924478c5b69cbaec2f0edf8d
Omkar-world <omkarverma8052979256@gmail.com>
2024-12-30 00:02:45 +0530

    TL commit

--- MavenProj01/src/main/java/com/nt/main/App.java
+++ MavenProj01/src/main/java/com/nt/main/App.java
@@ -10,6 +10,9 @@ public class App {
 	public int sub(int a,int b) {
 		return a-b;
 	}
+	public int mul(int a,int b) {
+		return a*b;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
